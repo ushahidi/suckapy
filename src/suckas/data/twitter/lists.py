ad213d573cd62c73e37eb51adfb9004f18e20d25
Jonathon Morgan <jonathon@goodattheinternet.com>
2014-05-19 15:36:01 -0500

    twitter sucka

---
+++ src/suckas/data/twitter/lists.py
@@ -0,0 +1,18 @@
+items = [
+  {
+    'slug': 'syria',
+    'owner_screen_name': 'jonathonmorgan'
+  },
+  {
+    'slug': 'libya',
+    'owner_screen_name': 'jonathonmorgan'
+  },
+  {
+    'slug': 'general-conflict',
+    'owner_screen_name': 'jonathonmorgan'
+  },
+  {
+    'slug': 'egypt',
+    'owner_screen_name': 'jonathonmorgan'
+  }
+]
--- tests/test_vdc_syria.py
+++ tests/test_vdc_syria.py
@@ -1,6 +1,7 @@
 from src.suckas import vdc_syria
 
 def test():
-    vdc_syria.suck('a','b','c')
+    #vdc_syria.suck('a','b','c')
 
-    assert 1 == 2
+    #assert 1 == 2
+    pass
