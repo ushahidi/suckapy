3bd62bc6b310dab5ff038484f69f74d5215250b4
Jonathon Morgan <jonathon@goodattheinternet.com>
2015-03-15 15:00:02 -0500

    removing some twitter hashtags

--- src/suckas/data/twitter/hashtags.py
+++ src/suckas/data/twitter/hashtags.py
@@ -2,40 +2,6 @@
 
 items = [
   {
-    'hashtag': '#idf',
-    'country': 'Palestine'
-  },
-  {
-    'hashtag': '#OccupyHongKong',
-    'country': "Hong Kong",
-    'tags': ['protest']
-  },
-  {
-    'hashtag': '#OccupyAdmiralty',
-    'country': "Hong Kong",
-    'tags': ['protest']
-  },
-  {
-    'hashtag': '#OccupyMongkong',
-    'country': "Hong Kong",
-    'tags': ['protest']
-  },
-  {
-    'hashtag': '#UmbrellaRevolution',
-    'country': "Hong Kong",
-    'tags': ['protest']
-  },
-  {
-    'hashtag': '#UmbrellaMovement',
-    'country': "Hong Kong",
-    'tags': ['protest']
-  },
-  {
-    'hashtag': '#HKstudentstrike',
-    'country': "Hong Kong",
-    'tags': ['protest']
-  },
-  {
     'hashtag': '#ebola',
     'country': '',
     'tags': ['disease','ebola']
